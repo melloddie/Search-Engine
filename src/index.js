6aea082326d40d7e506386a54aea122e4687c418
Melody <melodymmumo@gmail.com>
2024-03-14 11:09:06 +0300

    Added the icon

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
-
+  let iconElement = document.querySelector("#icon");
+  
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon">`;
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
@@ -31,7 +32,7 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
 
-  if (minutes <10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
